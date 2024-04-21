fc9a6fda40b15ff7e7da86559c716e54ac905db8
FaberVillarreal18 <faber.villarrealantequera@unicolombo.edu.co>
2024-04-21 02:00:19 -0500

    Guia 19

---
+++ Guia_19/src/guia_19/Guia_19.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guia_19;
+
+import gui.Ventanas;
+
+/**
+ *
+ * @author rodri
+ */
+public class Guia_19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventanas x = new Ventanas();
+        x.setVisible(true);
+    }
+    
+}
